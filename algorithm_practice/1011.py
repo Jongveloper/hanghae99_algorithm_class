228e99ffce7a5334eede0f20416416d28c8dcf08
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-06-15 23:01:38 +0900

    1011번 문제 작동방식!

--- algorithm_practice/1011.py
+++ algorithm_practice/1011.py
@@ -8,19 +8,6 @@ test = int(input())
 distance = 0
 for i in range(test):
     x, y = list(map(int, input().split(' ')))
-    # fast_move = x + 1
-    # move = x
-    # while True:
-    #     if x <= 0:
-    #         distance += 1
-    #         x += 1
-    #     if y - x >= y - x + 1:
-    #         fast_move
-    #     if y - x <= y - x + 1:
-    #         move
-    #     if y == 0:
-    #         print(distance)
-    #     break
     distance = y - x  # 거리
     count = 0  # 이동 횟수
     move = 1  # count 별 이동 가능한 거리
@@ -31,3 +18,25 @@ for i in range(test):
         if count % 2 == 0:  # count 가 2의 배수일 때,
             move += 1
     print(count)
+# ------- 작동 방식 -------- #
+# distance = 3 - 0 = 3
+# count = 0
+# move = 1
+# move_plus = 0
+# ------ while 문 -------- # 0 3 을 넣었을 때
+# while 0(move_plus) < 3(distance) : True
+# count = 1
+# 0(move_plus) + 1(move) = 1
+# 1 % 2 != 0 -> if 조건 통과못함!
+# while 1(move_plus) < 3(distance): True
+# count = 2
+# 1(move_plus) + 1(move) = 2
+# 2 % 2 == 0 -> if 조건 통과!
+# move = 2
+# while 2(move_plus) < 3(distance): True
+# count = 3
+# 2(move_plus) + 2(move) = 4
+# 4 % 2 == 0 -> if 조건 통과!
+# move = 3
+# while 3(move_plus) < 3(distance): False!
+# print(count) count = 3 이기 때문에 3이 출력됨
